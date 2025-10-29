b693a1982818140c4a8530cfccf0a048857504a5
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2025-10-28 20:33:25 -0930

    Create one.py

---
+++ one.py
@@ -0,0 +1,9 @@
+food = ["pizza", "cheese", "burger","coke"]
+dinner = ["breakfast", "lunch", "dinner"]
+cars = ["benz","bmw","Range rover","audi"]
+all_thins =[food,dinner,cars]
+print(all_thins)
+for i in all_thins:
+    for j in i:
+        print(j)
+
