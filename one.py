a9725d7d133fd4fece8645379b88efe26cc1d230
DZODZODZI  CEDRICK <cdzodzodzi@st.ug.edu.gh>
2025-12-04 14:15:09 +0000

    Fix variable name from all_thins to all_things

--- one.py
+++ one.py
@@ -1,8 +1,8 @@
 food = ["pizza", "cheese", "burger","coke"]
 dinner = ["breakfast", "lunch", "dinner"]
 cars = ["benz","bmw","Range rover","audi"]
-all_thins =[food,dinner,cars]
-print(all_thins)
+all_things =[food,dinner,cars]
+print(all_things)
 for i in all_thins:
     for j in i:
         print(j)
