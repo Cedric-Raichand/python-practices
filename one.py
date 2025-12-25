7562d92600ff58aef5ec72d684cb16c6e72c6402
DZODZODZI  CEDRICK <cdzodzodzi@st.ug.edu.gh>
2025-12-25 00:41:34 +0000

    Update one.py

--- one.py
+++ one.py
@@ -1,6 +1,6 @@
 food = ["Pizza", "cheese", "burger","coke"]
 dinner = ["Breakfast", "lunch", "dinner"]
-cars = ["Benz","bmw","Range rover","audi"]
+cars = ["Benz","bmw","Range rover","audi","ford"]
 all_things =[food,dinner,cars]
 print(all_things)
 for i in all_thins:
