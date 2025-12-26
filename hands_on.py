b47c49bc77c67fb76a01922d6701b6b8bc83c645
DZODZODZI  CEDRICK <cdzodzodzi@st.ug.edu.gh>
2025-12-26 09:17:44 +0000

    Update hands_on.py

--- hands_on.py
+++ hands_on.py
@@ -1,4 +1,3 @@
-
 utensils = {"knife","spoon","fork","ladel"}
 dishes = {"napkin","bowl","plate","cup"}
 utensils.update(dishes)
@@ -14,18 +13,18 @@ for i in dinner:
     print(i)
 print()
 
-capitals = {"usa":"washington dc",
-            "india":"new delhi",
-            "china":"new york",
-            "russia":"manager"}
+capitals = {"USA":"Washington dc",
+            "India":"New delhi",
+            "China":"New york",
+            "Russia":"Moscow"}
 
-print(capitals["russia"])
+print(capitals["Russia"])
 print(capitals.get("YUK"))
 print(capitals.keys())
 print(capitals.values())
 print(capitals.items())
 print(capitals)
-print(capitals.pop("china"))
+print(capitals.pop("China"))
 
 for key,values in capitals.items():
     print(key,values)
